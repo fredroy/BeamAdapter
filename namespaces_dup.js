6aa59d874541d42e77899130667209724986deb9
fredroy <fredroy@users.noreply.github.com>
2022-07-26 03:45:29 +0000

    Deploying to gh-pages from  @ fad24a195763262040d3214dc8eb8d1445b04d30 🚀

---
+++ namespaces_dup.js
@@ -0,0 +1,4 @@
+var namespaces_dup =
+[
+    [ "sofa", "namespacesofa.html", "namespacesofa" ]
+];
--- navtreedata.js
+++ navtreedata.js
@@ -22,12 +22,39 @@ for the JavaScript code in this file
 */
 var NAVTREE =
 [
-  [ "BeamAdapter", "index.html", ]
+  [ "BeamAdapter", "index.html", [
+    [ "Todo List", "todo.html", null ],
+    [ "Namespaces", "namespaces.html", [
+      [ "Namespace List", "namespaces.html", "namespaces_dup" ],
+      [ "Namespace Members", "namespacemembers.html", [
+        [ "All", "namespacemembers.html", null ],
+        [ "Functions", "namespacemembers_func.html", null ],
+        [ "Variables", "namespacemembers_vars.html", null ]
+      ] ]
+    ] ],
+    [ "Classes", "annotated.html", [
+      [ "Class List", "annotated.html", "annotated_dup" ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+      [ "Class Members", "functions.html", [
+        [ "All", "functions.html", null ],
+        [ "Functions", "functions_func.html", null ],
+        [ "Variables", "functions_vars.html", null ],
+        [ "Typedefs", "functions_type.html", null ]
+      ] ]
+    ] ],
+    [ "Files", "files.html", [
+      [ "File List", "files.html", "files_dup" ]
+    ] ]
+  ] ]
 ];
 
 var NAVTREEINDEX =
 [
-"index.html"
+".html",
+"classsofa_1_1component_1_1constraintset_1_1_unbuilt_generic_constraint_problem.html#a5730960856e57f8ac00d7432f7bfd64c",
+"classsofa_1_1component_1_1fem_1_1__beaminterpolation___1_1_beam_interpolation.html#a2cd3a9121bffc66213bf9f056fad8ea9",
+"classsofa_1_1component_1_1forcefield_1_1__adaptivebeamforcefieldandmass___1_1_adaptive_beam_force_field_and_mass.html#a2e447123c2639c737be660e6a5f3e439",
+"namespacemembers.html"
 ];
 
 var SYNCONMSG = 'click to disable panel synchronisation';
